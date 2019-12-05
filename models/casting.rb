c37f9420320428483ea0407919d0ded097262b10
Luca Sanz Charreun <lucasanz2471@gmail.com>
2019-12-05 15:09:18 +0000

    initial commit

---
+++ models/casting.rb
@@ -0,0 +1,12 @@
+class Casting
+
+  attr_reader :id,
+  attr_accessor :movie_id, :star_id, :fee
+
+  def initialize( options )
+    @id = options['id'].to_i if options['id'].to_i
+    @movie_id = options['movie_id'].to_i
+    @star_id = options['star_id'].to_i
+    @fee = options['fee']
+  end
+end
